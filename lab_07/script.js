d5431847d762b9c9b3f360256e80b695a2986af4
ann-bychyshyn <annabychyshynanna@gmail.com>
2024-11-06 10:13:32 +0200

    rename the lab 07 and 08

--- lab_07/script.js
+++ lab_07/script.js
@@ -1,28 +1,47 @@
+let balance = 100;  
+const minNumber = 1;
+const maxNumber = 5;
+const maxAttempts = 3; 
 
-let a = Number(prompt("Enter the first number"));
-let b = Number(prompt("Enter the secind number"));
-let action = prompt("Chose the action summary - 1, substraction - 2, multiplation - 3, dividing - 4");
-if (action == 1 ) {
-  summary = a + b;
-  alert("summary = " + summary);
-}
-else if (action == 2) {
-  substraction = a - b;
-  alert("substraction = " + substraction);
-}
-else if (action == 3) {
-  multiplation = a * b;
-  alert("multiplation" + multiplation);
-}
-else if(action == 4) {
-  if (b == 0) {
-    alert("Dividing by 0, not working");
+function guessNumber() {
+    const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+    for (let i = 1; i <= maxAttempts; i++) {
+        const userGuess = parseInt(prompt(`Виберіть число від 1 до 5 (Спроба ${i}/${maxAttempts})`));
+
+        if (userGuess === randomNumber) {
+            let reward;
+            if (i === 1) {
+                reward = 40;
+            } else if (i === 2) {
+                reward = 20;
+            } else {
+                reward = 10;
+            }
+
+            balance += reward;
+            alert(`Ви вгадали число ${randomNumber}! Ви виграли $${reward}. Ваш баланс: $${balance}`);
+            askToRestart();  
+            return; 
+        } else {
+            alert(`Ви не вгадали! Спроба ${i}/${maxAttempts}.`);
+        }
+
+        if (i === maxAttempts) {
+            balance -= 20;  
+            alert(`Ви не вгадали число за 3 спроби! Ви втратили $10. Правильне число було ${randomNumber}. Ваш баланс: $${balance}`);
+            askToRestart();  
+        }
     }
-  else {
-    dividing = a/b;
-  alert("dividing = "+ dividing);
-  }
 }
-else {
-  alert("This is not the rigth option(");
+
+function askToRestart() {
+    const playAgain = confirm(`Бажаєш зіграти ще раз? Ну давай можливо цього разу заробиш більшеб або ні:) Твій баланс зараз ` + balance+'$');
+    if (playAgain) {
+        guessNumber(); 
+    } else {
+        alert("Дякуємо за гру! Будемо чекати на вас, Ваш остаточний баланс: $" + balance);
+    }
 }
+
+guessNumber();
